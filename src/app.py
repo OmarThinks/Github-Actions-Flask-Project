80839822498338db45af1881d10e832077ff0405
Omar Magdy <contact.parmag@gmail.com>
2021-06-05 19:08:45 +0200

    app.py, test_.py: fixed lints

--- src/app.py
+++ src/app.py
@@ -1,11 +1,11 @@
 from flask import Flask
-import os
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-	return {"success":True, "message":"Hello, World!"}
+	return {"success": True, "message": "Hello, World!"}
 
 
 if __name__ == "__main__":
--- src/test_.py
+++ src/test_.py
@@ -3,19 +3,18 @@ import json
 
 from .app import app
 
-class CITestCase(unittest.TestCase):
 
+class CITestCase(unittest.TestCase):
     def setUp(self):
         self.app = app
         self.client = self.app.test_client
-        
 
     def test_001_test(self):
         response = self.client().get("/")
-        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.status_code, 200)
         data = json.loads(response.data)
-        self.assertEqual(data["success"],True)
-        self.assertEqual(data["message"],"Hello, World!")
+        self.assertEqual(data["success"], True)
+        self.assertEqual(data["message"], "Hello, World!")
 
 
 # Make the tests conveniently executable
